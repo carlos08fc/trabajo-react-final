7cc2241ec6adda15bbee6578aa51b48e142e837d
cafccamacho <cafccamacho08@gmail.com>
2020-03-12 08:33:40 -0600

    se realizo la proteccion de rutas (paso 3)

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 // @components
-//import Layout from "../Layout";
 //import Dashboard from "../Dashboard";
 import Login from "./pag/login";
 // @utilities
-//import ProtectedRoute from "../../utilities/ProtectedRouter";
+import ProtectedRoute from "./configuracion/ProtectedRouter";
 
 //export const AuthContext = React.createContext();
 
 export default function App() {
   return (
     <Switch>
-      <Route path="/home"></Route>
+      <ProtectedRoute path="/home"></ProtectedRoute>
       <Route path="/login" component={Login} />
     </Switch>
   );
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./configuracion/useAuth";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </BrowserRouter>,
   rootElement
 );
